ab9cb7f0011b01c32abe7033f7eb64fd3318e338
Johan Selin <johanselin99@gmail.com>
2022-01-22 01:31:27 +0100

    refactor and new assert

--- tests/test_pixelize.py
+++ tests/test_pixelize.py
@@ -4,15 +4,15 @@ import pixelize
 
 class MyTestCase(unittest.TestCase):
 
-    def test_make_matrix(self):
-        rows, cols = pixelize.make_matrix(250, 383, 10)
+    def test_get_rows_cols(self):
+        rows, cols = pixelize.get_rows_cols(250, 383, 10)
         self.assertEqual(rows, 25)
         self.assertEqual(cols, 38)
 
     def test_average_color(self):
         colors = [(100, 100, 100, 255), (200, 200, 200, 255), (300, 300, 500, 255), (10000, 10000, 10000, 0)]
         result = pixelize.average_color(colors)
-        print(result)
+        self.assertEqual(result, (200, 200, 267, 255))
 
 
 if __name__ == '__main__':
