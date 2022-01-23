7fc8fecaf8158b2c9f888868145c6ad9fe1c9b32
Johan Selin <johanselin99@gmail.com>
2022-01-23 02:09:01 +0100

    Bugfix, functional decomposition, progress indication

--- pixelize.py
+++ pixelize.py
@@ -14,7 +14,7 @@ def get_rows_cols(width, height, pixel_size):
 
 def average_color(rgba_values: list):
     avg = {'red': 0, 'green': 0, 'blue': 0, 'count': 0}
-    has_transparency = len(rgba_values) == 4
+    has_transparency = (len(rgba_values[0]) == 4)
     for RGBA in rgba_values:
         if not has_transparency or RGBA[3]:
             avg['red'] += RGBA[0]
@@ -31,27 +31,38 @@ def average_color(rgba_values: list):
     return avg_of('red'), avg_of('green'), avg_of('blue'), 255
 
 
-def do(pixel_size):
-    with Image.open('images/vangogh.png') as my_image:
+def do(pixel_size, image_path):
+    with Image.open(image_path) as my_image:
         rows, cols = get_rows_cols(my_image.width, my_image.height, pixel_size=pixel_size)
         for i in range(rows):
             for j in range(cols):
                 colors = []
-                for top in range(pixel_size):
-                    for left in range(pixel_size):
-                        xy = ((i*pixel_size) + top, (j*pixel_size) + left)
-                        colors.append(my_image.getpixel(xy))
+                for pixel in loop_over_chunk(i, j, pixel_size):
+                    colors.append(my_image.getpixel(pixel))
                 new_color = average_color(colors)
-                for top in range(pixel_size):
-                    for left in range(pixel_size):
-                        xy = ((i * pixel_size) + top, (j * pixel_size) + left)
-                        if new_color:
-                            my_image.putpixel(xy, new_color)
+                if new_color:
+                    for pixel in loop_over_chunk(i, j, pixel_size):
+                        my_image.putpixel(pixel, new_color)
+            yield percent(i, rows)
         my_image = my_image.crop((0, 0, rows*pixel_size, cols*pixel_size))
+
         my_image.show()
 
+
+def loop_over_chunk(i, j, pixel_size):
+    for top in range(pixel_size):
+        for left in range(pixel_size):
+            yield (i * pixel_size) + top, (j * pixel_size) + left
+
+
+def percent(part, whole):
+    return round((part / whole) * 100, 1)
+
+
 if __name__ == '__main__':
-    do(15)
+    for completion in do(5, 'images/mario.png'):
+        print(f"{completion}% finished")
+    # do(5, 'images/furnaceman.png')
 
 
 
